380249000ca2b94693c656bd8ad22524233d5dbf
Siloda <Dopejob24@gmail.com>
2022-03-26 01:55:25 +0530

    12
    123

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -13,36 +13,18 @@ public class Player : MonoBehaviour
     int criticalStrike;
     int less;
 
-<<<<<<< Updated upstream
+ 
      
 
     // Update is called once per frame
     void Update()
-    { 
-
-        if (currentHealth <= 0)
-=======
-    // Start is called before the first frame update
-    void Start()
     {
-        health = 10;
-        currentHealth = health;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            health -= 1;
-         
-            Debug.Log(health);
-        }
-        else
->>>>>>> Stashed changes
-        {
+        if (currentHealth <= 0) 
+ 
+     
             Debug.Log("GameOver");
         }
     }
 
-}
+ 
