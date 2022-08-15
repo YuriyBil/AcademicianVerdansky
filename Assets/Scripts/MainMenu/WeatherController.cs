8585358c061d79a14185b65b46479561e5916335
Sedjex <kathshem@gmail.com>
2022-08-15 14:41:26 +0300

    [Weather] upd 15/08 need to fix

--- Assets/Scripts/MainMenu/WeatherController.cs
+++ Assets/Scripts/MainMenu/WeatherController.cs
@@ -1,26 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+//[RequireComponent(typeof(Image))]
 public class WeatherController : MonoBehaviour
 {
-    public GameObject Snow;
+    public float Speed = 1f;
+    public int FrameRate = 30;
+    public bool Loop = false;
+    public Image _snowImage = null;
 
-    void KeyCodeInput()
+    private Sprite[] _snowSprites = null;
+    private float _snowTimePerFrame = 0f;
+    private float _snowElapsedTime = 0f;
+    private int _snowCurrentFrame = 0;
+
+    void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            Snow.SetActive(true);
-        }
+        enabled = false;
+        LoadSpriteSheet();
+    }
 
-        else if (Input.GetKeyDown(KeyCode.X))
+    private void LoadSpriteSheet()
+    {
+        _snowSprites = Resources.LoadAll<Sprite>("snow-Sheet1");
+        if (_snowSprites != null && _snowSprites.Length > 0)
         {
-            Snow.SetActive(false);
+            _snowTimePerFrame = 1f / FrameRate;
+            Play();
+        }
+        else
+        {
+            Debug.LogError("Failed to load sprite sheet");
         }
     }
 
-    void Update()
+    public void Play()
     {
-        KeyCodeInput();
+        enabled = true;
+    }
+
+    public void Update()
+    {
+        _snowElapsedTime += Time.deltaTime * Speed;
+        if (_snowElapsedTime >= _snowTimePerFrame)
+        {
+            _snowElapsedTime = 0f;
+            ++_snowCurrentFrame;
+            SetSprite();
+            if (_snowCurrentFrame >= _snowSprites.Length)
+            {
+                if (Loop)
+                {
+                    _snowCurrentFrame = 0;
+                }
+                else
+                {
+                    enabled = false;
+                }
+            }
+        }
+    }
+
+    private void SetSprite()
+    {
+        if (_snowCurrentFrame >= 0 && _snowCurrentFrame < _snowSprites.Length)
+        {
+            _snowImage.sprite = _snowSprites[_snowCurrentFrame];
+        }
     }
 }
