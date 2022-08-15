2b90a4308f752c618cffe91e6c081d407d4a6151
Sedjex <kathshem@gmail.com>
2022-08-15 16:52:32 +0300

    [Weather] snowfall fixed

--- Assets/Scripts/MainMenu/WeatherController.cs
+++ Assets/Scripts/MainMenu/WeatherController.cs
@@ -6,68 +6,23 @@ using UnityEngine.UI;
 //[RequireComponent(typeof(Image))]
 public class WeatherController : MonoBehaviour
 {
-    public float Speed = 1f;
-    public int FrameRate = 30;
-    public bool Loop = false;
-    public Image _snowImage = null;
+    public GameObject Snow;
 
-    private Sprite[] _snowSprites = null;
-    private float _snowTimePerFrame = 0f;
-    private float _snowElapsedTime = 0f;
-    private int _snowCurrentFrame = 0;
-
-    void Start()
+    void KeyCodeInput()
     {
-        enabled = false;
-        LoadSpriteSheet();
-    }
-
-    private void LoadSpriteSheet()
-    {
-        _snowSprites = Resources.LoadAll<Sprite>("snow-Sheet1");
-        if (_snowSprites != null && _snowSprites.Length > 0)
+        if (Input.GetKeyDown(KeyCode.Z))
         {
-            _snowTimePerFrame = 1f / FrameRate;
-            Play();
+            Snow.SetActive(true);
         }
-        else
+
+        else if (Input.GetKeyDown(KeyCode.X))
         {
-            Debug.LogError("Failed to load sprite sheet");
+            Snow.SetActive(false);
         }
     }
 
-    public void Play()
+    void Update()
     {
-        enabled = true;
-    }
-
-    public void Update()
-    {
-        _snowElapsedTime += Time.deltaTime * Speed;
-        if (_snowElapsedTime >= _snowTimePerFrame)
-        {
-            _snowElapsedTime = 0f;
-            ++_snowCurrentFrame;
-            SetSprite();
-            if (_snowCurrentFrame >= _snowSprites.Length)
-            {
-                if (Loop)
-                {
-                    _snowCurrentFrame = 0;
-                }
-                else
-                {
-                    enabled = false;
-                }
-            }
-        }
-    }
-
-    private void SetSprite()
-    {
-        if (_snowCurrentFrame >= 0 && _snowCurrentFrame < _snowSprites.Length)
-        {
-            _snowImage.sprite = _snowSprites[_snowCurrentFrame];
-        }
+        KeyCodeInput();
     }
 }
