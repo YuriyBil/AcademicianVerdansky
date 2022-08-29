adb0b4030b51db742345f517fe2faba304ff89fc
Sedjex <kathshem@gmail.com>
2022-08-29 14:21:15 +0300

    [PlayerStats] Stats at Medical location

---
+++ Assets/PlayerStatsTexting.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerStatsTexting : MonoBehaviour
+{
+    // void Awake()
+    // {
+    //     TextMeshPro textmeshPro = GetComponent<TextMeshPro>();
+    //     textmeshPro.faceColor = new Color32(255, 128, 0, 255);
+    // }
+}
--- Assets/Scripts/Medicine.cs
+++ Assets/Scripts/Medicine.cs
@@ -10,13 +10,20 @@ public class Medicine : MonoBehaviour
     public Button MedNigtht;
     public Button MedDay;
 
+    public Button Profile;
+    public Button ExitProfile;
+
     public GameObject NightAnimation;
     public GameObject DayAnimation;
+
+    public GameObject ProfileOfPlayer;
 
     void OnEnable()
     {
         MedNigtht.onClick.AddListener(NightAnim);
         MedDay.onClick.AddListener(DayAnim);
+        Profile.onClick.AddListener(ShowProfile);
+        ExitProfile.onClick.AddListener(CancelShowProfile);
     }
 
     void NightAnim()
@@ -29,6 +36,19 @@ public class Medicine : MonoBehaviour
     {
         DayAnimation.SetActive(true);
         NightAnimation.SetActive(false);
+    }
+
+    void ShowProfile()
+    {
+        ProfileOfPlayer.SetActive(true);
+        DayAnimation.SetActive(false);
+        NightAnimation.SetActive(false);
+    }
+
+    void CancelShowProfile()
+    {
+        ProfileOfPlayer.SetActive(false);
+        DayAnim();
     }
 
     void Update()
