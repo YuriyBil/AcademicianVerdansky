4a67f223f9336aea9839a1e54a322cfa49387ccd
Sedjex <kathshem@gmail.com>
2022-08-29 15:06:15 +0300

    [PlayerStats] value fields to script

--- Assets/PlayerStatsTexting.cs
+++ Assets/PlayerStatsTexting.cs
@@ -5,9 +5,38 @@ using TMPro;
 
 public class PlayerStatsTexting : MonoBehaviour
 {
-    // void Awake()
-    // {
-    //     TextMeshPro textmeshPro = GetComponent<TextMeshPro>();
-    //     textmeshPro.faceColor = new Color32(255, 128, 0, 255);
-    // }
+
+    public TextMeshProUGUI HealthValue;
+    public TextMeshProUGUI EnergyValue;
+    public TextMeshProUGUI ThermoValue;
+    public TextMeshProUGUI FoodValue;
+    public TextMeshProUGUI WaterValue;
+    public TextMeshProUGUI SuguardValue;
+    public TextMeshProUGUI ProteinsValue;
+    public TextMeshProUGUI FatsValue;
+    public TextMeshProUGUI CarbsValue;
+    public TextMeshProUGUI AttackValue;
+    public TextMeshProUGUI BlockValue;
+    public TextMeshProUGUI DodgeValue;
+    public TextMeshProUGUI CriticalValue;
+
+
+    void Start()
+    {
+        HealthValue.text = "100/100";
+        EnergyValue.text = "100/100";
+        ThermoValue.text = "100/100";
+        FoodValue.text = "100/100";
+        WaterValue.text = "100/100";
+        SuguardValue.text = "100";
+        ProteinsValue.text = "100";
+        FatsValue.text = "100";
+        CarbsValue.text = "100";
+        AttackValue.text = "2-8";
+        BlockValue.text = "7%";
+        DodgeValue.text = "6%";
+        CriticalValue.text = "5%";
+
+    }
+
 }
