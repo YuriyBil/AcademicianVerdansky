991f4e0f0961cb019945740dad5baa9bcab33fab
Sedjex <kathshem@gmail.com>
2022-09-13 13:49:03 +0300

    [TestScene] Scene1 pref & particle snow - draft

--- Assets/Scripts/Medicine.cs
+++ Assets/Scripts/Medicine.cs
@@ -3,46 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.Video;
 using System;
 
 public class Medicine : MonoBehaviour
 {
-    public Button MedNigtht;
-    public Button MedDay;
+    public Button MedNigthtButton;
+    public Button MedDayButton;
 
     public Button Profile;
     public Button ExitProfile;
 
-    public GameObject NightAnimation;
-    public GameObject DayAnimation;
+    //public VideoPlayer Night;
+    public VideoPlayer Day;
+    public VideoClip MedDay;
+    public VideoClip MedNight;
 
     public GameObject ProfileOfPlayer;
 
     void OnEnable()
     {
-        MedNigtht.onClick.AddListener(NightAnim);
-        MedDay.onClick.AddListener(DayAnim);
+        MedNigthtButton.onClick.AddListener(NightAnim);
+        MedDayButton.onClick.AddListener(DayAnim);
         Profile.onClick.AddListener(ShowProfile);
         ExitProfile.onClick.AddListener(CancelShowProfile);
     }
 
     void NightAnim()
     {
-        DayAnimation.SetActive(false);
-        NightAnimation.SetActive(true);
+        Day.Stop();
+        Day.clip = MedNight;
+        Day.Play();
     }
 
     void DayAnim()
     {
-        DayAnimation.SetActive(true);
-        NightAnimation.SetActive(false);
+        Day.Stop();
+        Day.clip = MedDay;
+        Day.Play();
     }
 
     void ShowProfile()
     {
         ProfileOfPlayer.SetActive(true);
-        DayAnimation.SetActive(false);
-        NightAnimation.SetActive(false);
+        // Day.Stop();
+        // Night.Stop();
     }
 
     void CancelShowProfile()
